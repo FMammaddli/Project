b2e84576dc05257ce11f3daa4cb7b94b83a15a13
Farid <fmammadli13936@ada.edu.az>
2024-12-16 19:32:54 +0400

    Second

---
+++ js/script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const category = 'toys';
+const Jau = "basqwas";
+console.log(`https://someurl.com/${category}/5/${Jau}`);
+
+const userName = prompt("Whats your name?", "");
+
+alert(`Hello! ${userName}`);
