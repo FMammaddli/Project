07d6013de6eeef116660d8d09b034fc7f4297546
Farid <fmammadli13936@ada.edu.az>
2024-12-16 19:30:08 +0400

    Second Commit

--- js/script.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-const category = 'toys';
-const Jau = "basqwas";
-console.log(`https://someurl.com/${category}/5/${Jau}`);
-
-const userName = prompt("Whats your name?", "");
-
-alert(`Hello! ${userName}`);
